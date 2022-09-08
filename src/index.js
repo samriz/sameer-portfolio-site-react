d9ff0471474adac1d4b45a2e0e7d628947d6ba4e
samriz <samriz@users.noreply.github.com>
2022-09-08 09:56:20 -0500

    Update index.js
    Added import statement for bootstrap.css.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './index.css';
 import PageTemplate from './pagetemplate';
 
