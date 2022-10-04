48a0bd278a33abb0cb1fee5db36cd5de045200b2
samriz <samriz@users.noreply.github.com>
2022-10-04 09:46:27 -0500

    Started work on a main site page/header. Added a "DOWNLOAD RESUME" button in About page. Added "misc" folder with resume src folder.

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+
+export default class SiteHeader extends React.Component
+{
+    render()
+    {
+        return (
+            <header>
+                <div>
+                    <span>Sameer Rizvi</span>
+                </div>
+            </header>
+        );
+    }
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PageTemplate from "../pagetemplate";
 import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
-import resume from "./SameerRizviResume.pdf";
+import Resume from "../misc/SameerRizviResume.pdf";
 import Footer from "../footer";
 import AboutPage from "./about";
+import SiteHeader from "../components/header";
 
 export default class HomePage extends React.Component
 {
@@ -12,6 +13,7 @@ export default class HomePage extends React.Component
     {
         return (
             <>
+                <SiteHeader/>
                 <PageTemplate 
                     intro_content={<h1>Sameer Rizvi</h1>}
                     main_content={this.homePageBody()}
@@ -29,7 +31,7 @@ export default class HomePage extends React.Component
             <p>Welcome to my official Web Portfolio.</p>
             <p>Here, you'll find information pertaining to my academic and professional experiences. This includes, but is not limited to, the following:</p>
             <ul>
-                <li>My <Link to={resume} title={"Resume"} target={"_blank"} rel={"author"}>resume <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                <li>My <Link to={Resume} title={"Resume"} target={"_blank"} rel={"author"}>resume <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
                 <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
                 </svg></Link>.</li>
