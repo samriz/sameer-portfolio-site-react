a9f01a6093661be3300250fa662b75be6d4e6526
samriz <samriz@users.noreply.github.com>
2022-10-07 10:51:34 -0500

    Update home.js
    Fixed links to about and contact pages.

--- src/pages/home.js
+++ src/pages/home.js
@@ -38,10 +38,10 @@ export default class HomePage extends React.Component
             <ul>
                 <li>My <Link to={Resume} title={"Resume"} target={"_blank"} rel={"author"}>resume <ExternalUrlArrow/></Link>.</li>
                 <li>An 
-                    <HashLink to={"/#aboutHeader"} title="About" rel="author"> about</HashLink> section, detailing my academic and professional history.
+                    <HashLink to={"/#about"} title="About" rel="author"> about</HashLink> section, detailing my academic and professional history.
                 </li>
                 <li>My <a href={"https://github.com/samriz"} target={"_blank"} title={"GitHub"} rel={"noopener noreferrer"}>GitHub <ExternalUrlArrow/></a>.</li>
-                <li>A way to <HashLink to={"/#contactHeader"} title={"Contact"} rel={"author"}>contact</HashLink> me.</li>
+                <li>A way to <HashLink to={"/#contact"} title={"Contact"} rel={"author"}>contact</HashLink> me.</li>
                 <li>
                     <HashLink smooth to={"/#summaryProfessionalSamples"} onClick={this.openProfessionalSamples}>Links</HashLink> to some of my professional work.
                 </li>
