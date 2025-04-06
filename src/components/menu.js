6ee5ac64500afa9f2f3e8fc106f482e0fdb302d3
samriz <samriz@users.noreply.github.com>
2025-04-06 12:18:41 -0500

    changes to menuitems. Added DesktopMenuItem

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,28 +1,27 @@
 import React from "react";
-import { MenuItem } from "./menuitem";
+import { DesktopMenuItem } from "./menuitem";
 
 export default class Menu extends React.Component
 {
     //renderMenuItem(href, val) {return (<MenuItem href={href} text={val}/>);}
 
-    render()
-    {
+    render() {
         return (
-          <nav className={"menu"}>
-            <div id={"divMenuItems"}>
-              <MenuItem
-                href={"/#home"}
-                text={"sameer"}
+          <nav className="menu">
+            <div id="divMenuItems">
+              <DesktopMenuItem
+                href="/#home"
+                text="sameer"
               />
               <span style={{color: "white"}}>|</span>
-              <MenuItem
-                href={"/#about"}
-                text={"about"}
+              <DesktopMenuItem
+                href="/#about"
+                text="about"
               />
               <span style={{color: "white"}}>|</span>
-              <MenuItem
-                href={"/#contact"}
-                text={"contact"}
+              <DesktopMenuItem
+                href="/#contact"
+                text="contact"
               />
             </div>
           </nav>
--- src/components/menuitem.js
+++ src/components/menuitem.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { HashLink } from 'react-router-hash-link';
 
-export class MenuItem extends React.Component
+class MenuItem extends React.Component
 {
-    render() {return (<HashLink className={"menuItem"} smooth to={this.props.href}>{this.props.text}</HashLink>);}
+    render() {return (<HashLink className={this.props.className} smooth to={this.props.href}>{this.props.text}</HashLink>);}
+}
+
+/* export class MobileMenuItem extends React.Component
+{
+    render() {return (<HashLink className={"mobileMenuItem"} smooth to={this.props.href}>{this.props.text}</HashLink>);}
+} */
+
+export class DesktopMenuItem extends React.Component
+{
+    render() {
+        return (
+            <MenuItem 
+                className="menuItem" 
+                href={this.props.href} 
+                text={this.props.text}
+            />
+        );
+    }
 }
 
 export class MobileMenuItem extends React.Component
 {
-    render() {return (<HashLink className={"mobileMenuItem"} smooth to={this.props.href}>{this.props.text}</HashLink>);}
+    render() {
+        return (
+            <MenuItem 
+                className="mobileMenuItem" 
+                href={this.props.href} 
+                text={this.props.text}
+            />
+        );
+    }
 }
